a78e4f9f9ca3c418059c07560cf961136a3916f7
Fadhlan Rahimi <fadhlanr.hf@gmail.com>
2022-06-28 15:52:53 +0800

    delete server node modules

--- db.js
+++ db.js
@@ -22,12 +22,7 @@ const prodConfig = process.env.DATABASE_URL
 module.exports = knex({
   client: 'postgres',
   connection: {
-    connectionString: process.env.NODE_ENV === "production" ? devConfig : prodConfig
+    connectionString: prodConfig
   }
 })
-
-// module.exports = knex({
-//   client: 'postgres',
-//   connectionString: process.env.NODE_ENV === "production" ? devConfig : prodConfig
-// })
 
